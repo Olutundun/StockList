1f698849f10f44e1deed72ee26dfca4e379c0d46
Your Name <tundun.deborah@gmail.com>
2020-03-05 18:08:53 -0500

    "initial commit"

---
+++ src/app/StockItem.java
@@ -0,0 +1,86 @@
+package app;
+
+public class StockItem implements Comparable<StockItem> {
+    private final String name;
+    private double price;
+    private int quantityStock;
+
+    public StockItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+        this.quantityStock = 0;
+    }
+
+    // overload the constructor so that the items can be added with or without the
+    // quantity stock
+    public StockItem(String name, double price, int quantityStock) {
+        this.name = name;
+        this.price = price;
+        this.quantityStock = quantityStock;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        if (price > 0.0) {
+            this.price = price;
+        }
+    }
+
+    public int getQuantityStock() {
+        return this.quantityStock;
+    }
+
+    // allows stock to be increased or decrease
+    public void adjustStock(int quantity) {
+        int newQuantity = this.quantityStock + quantity;
+        if (newQuantity >= 0) {
+            this.quantityStock = newQuantity;
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        System.out.println("Entering stockItem.equals");
+        // test to see if it's the same instance of the object in memory
+        if (obj == this) {
+            return true;
+        }
+        // check if obj is null and if it's the same class
+        if ((obj == null) || (obj.getClass() != this.getClass())) {
+            return false;
+        }
+        // comparing both names and return the standard string equals
+        String objName = ((StockItem) obj).getName();
+        return this.name.equals(objName);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.name.hashCode() + 31;
+    }
+
+    @Override
+    public int compareTo(StockItem o) {
+        System.out.println("Entering stockItem.compareTo");
+        if (this == o) {
+            return 0;
+        }
+        if (o != null) {
+            return this.name.compareTo(o.getName());
+        }
+        throw new NullPointerException();
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " : price " + this.price;
+    }
+
+}
