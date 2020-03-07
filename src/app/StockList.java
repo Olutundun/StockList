dfb9b7400a3f53579dccc8b14161dc18e4a64c1e
Your Name <tundun.deborah@gmail.com>
2020-03-07 17:14:10 -0500

    "added reserve and unreserve options"

--- src/app/StockList.java
+++ src/app/StockList.java
@@ -5,60 +5,83 @@ import java.util.TreeMap;
 import java.util.Map;
 
 public class StockList {
-   private final Map<String, StockItem> list;
+    private final Map<String, StockItem> list;
 
-   public StockList() {
-       this.list = new TreeMap<>();
-   }
-    
-   public int addStock(StockItem item) {
-       if (item != null) {
-           //check if already have quantities of this item
-           StockItem inStock = list.getOrDefault(item.getName(), item);
-           //if there are already stocks on this item adjust the quantity
-           if (inStock != item) {
-             item.adjustStock(inStock.quantityInStock());
-           }
-           list.put(item.getName(), item);
-           return item.quantityInStock();
-       }
-       return 0;
-   }
+    public StockList() {
+        this.list = new TreeMap<>();
+    }
 
-   public int sellStock(String item, int quantity) {
-       StockItem inStock = list.getOrDefault(item, null);
+    public int addStock(StockItem item) {
+        if (item != null) {
+            // check if already have quantities of this item
+            StockItem inStock = list.getOrDefault(item.getName(), item);
+            // if there are already stocks on this item adjust the quantity
+            if (inStock != item) {
+                item.adjustStock(inStock.availableQuantity());
+            }
+            list.put(item.getName(), item);
+            return item.availableQuantity();
+        }
+        return 0;
+    }
 
-       if ((inStock != null) && (inStock.quantityInStock() >= quantity) && (quantity > 0)) {
-            inStock.adjustStock(-quantity);
-            return quantity;
-       }
-       //not in stock or the quantity passed is < 0
-       return 0;
-   }
+    public int sellStock(String item, int quantity) {
+        // checks if items exists in the list and quantity is > 0
+        StockItem inStock = list.get(item);
+        if ((inStock != null) && (quantity > 0)) {
+            return inStock.finaliseStock(quantity);
+        }
+        return 0;
+        // StockItem inStock = list.getOrDefault(item, null);
 
-   public StockItem get(String key) {
-       return list.get(key);
-   }
-   //returns the items
-   public Map<String, StockItem> Items() {
-       return Collections.unmodifiableMap(list);
-   }
+        // if ((inStock != null) && (inStock.availableQuantity() >= quantity) &&
+        // (quantity > 0)) {
+        // inStock.adjustStock(-quantity);
+        // return quantity;
+        // }
+        // // not in stock or the quantity passed is < 0
+        // return 0;
+    }
 
-   @Override
-   //this is intended for debugging
-   public String toString() {
-       //returns a complete stock list
-       String s = "\nStock List:\n";
-       double totalCost = 0.0;
-       for(Map.Entry<String, StockItem> item : list.entrySet()) {
-           StockItem stockItem = item.getValue();
-           double itemValue = stockItem.getPrice() * stockItem.quantityInStock();
-           s = s + stockItem + ". There are " + stockItem.quantityInStock() + " in stock. Value of items: ";
-           s = s + String.format("%.2f",itemValue) + "\n";
-           totalCost += itemValue;
-       }
-       return s + "Total stock value " + String.format("%.2f", totalCost);
-   }
+    public int reserveStock(String item, int quantity) {
+        StockItem inStock = list.get(item);
+        if ((inStock != null) && (quantity > 0)) {
+            return inStock.reserveStock(quantity);
+        }
+        return 0;
+    }
 
+    public int unreserveStock(String item, int quantity) {
+        StockItem inStock = list.get(item);
+        if ((inStock != null) && (quantity > 0)) {
+            return inStock.unreserveStock(quantity);
+        }
+        return 0;
+    }
+
+    public StockItem get(String key) {
+        return list.get(key);
+    }
+
+    // returns the items
+    public Map<String, StockItem> Items() {
+        return Collections.unmodifiableMap(list);
+    }
+
+    @Override
+    // this is intended for debugging
+    public String toString() {
+        // returns a complete stock list
+        String s = "\nStock List:\n";
+        double totalCost = 0.0;
+        for (Map.Entry<String, StockItem> item : list.entrySet()) {
+            StockItem stockItem = item.getValue();
+            double itemValue = stockItem.getPrice() * stockItem.availableQuantity();
+            s = s + stockItem + ". There are " + stockItem.availableQuantity() + " in stock. Value of items: ";
+            s = s + String.format("%.2f", itemValue) + "\n";
+            totalCost += itemValue;
+        }
+        return s + "Total stock value " + String.format("%.2f", totalCost);
+    }
 
 }
